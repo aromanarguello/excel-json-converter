e57f380ca66161fca7f6dbaa34f2df529f3e1c25
Alejandro <aromanarguello@gmail.com>
2019-12-16 23:29:39 -0600

    structured output

--- converter.py
+++ converter.py
@@ -8,11 +8,12 @@ sh = wb.sheet_by_index(0)
 data_list = []
 for rownum in range(1, sh.nrows):
     data = OrderedDict()
-    print(data)
     row_values = sh.row_values(rownum)
-    print(row_values)
-    data['pattern'] = row_values[0]
-    data['response'] = row_values[1]
+    print(row_values[2], row_values[3])
+    data['code'] = row_values[0]
+    data['name'] = row_values[1]
+    data['price'] = row_values[2]
+    data['currency'] = row_values[3]
     data_list.append(data)
 j = json.dumps(data_list)
 
