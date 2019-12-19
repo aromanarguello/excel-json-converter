75311c7a5123d3de8d8806981c28fa6378a627d0
Alejandro <aromanarguello@gmail.com>
2019-12-18 22:44:06 -0600

    made into function

--- converter.py
+++ converter.py
@@ -1,21 +1,27 @@
-import xlrd
 from collections import OrderedDict
+import xlrd
 
 import simplejson as json
 
 wb = xlrd.open_workbook('listaprecios.xlsx')
 sh = wb.sheet_by_index(0)
 data_list = []
-for rownum in range(1, sh.nrows):
-    data = OrderedDict()
-    row_values = sh.row_values(rownum)
-    if row_values[1] and row_values[1]:
-        data['code'] = row_values[0]
-        data['name'] = row_values[1]
-        data['price'] = row_values[2]
-        data['currency'] = row_values[3]
-        data_list.append(data)
-j = json.dumps(data_list)
+def generate_json():
+    '''
+    Generates json file from xlsx
+    '''
+    for rownum in range(1, sh.nrows):
+        data = OrderedDict()
+        row_values = sh.row_values(rownum)
+        if row_values[1] and row_values[1]:
+            data['code'] = row_values[0]
+            data['name'] = row_values[1]
+            data['price'] = row_values[2]
+            data['currency'] = row_values[3]
+            data_list.append(data)
+    json_file = json.dumps(data_list)
+    with open('data.json', 'w') as file:
+        file.write(json_file)
+        print('data.json file generated')
 
-with open('data.json', 'w') as f:
-    f.write(j)
+generate_json()
